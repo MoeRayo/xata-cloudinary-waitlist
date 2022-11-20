fd46a5331ffb6e9ab42bb6f61ad2dfc6f6aa38fb
moerayo <moronfolu.motunrayo1@gmail.com>
2022-11-20 18:55:42 +0100

    fix: cloudinary image render fix

--- src/components/TheCloudinaryBackground.vue
+++ src/components/TheCloudinaryBackground.vue
@@ -1,3 +1,5 @@
 <template>
-  <cld-image publicId="hannah-busing-unsplash" dpr="auto" class="dn db-l w-50-ns vh-100" quality= "auto" />
+  <cld-image publicId="hannah-busing-unsplash" dpr="auto" class="dn db-l w-50-ns vh-100" quality= "auto">
+    <cld-transformation flags="rasterize" />
+  </cld-image>
 </template>
